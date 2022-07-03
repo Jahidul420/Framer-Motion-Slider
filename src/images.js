fa8452786273b23e3d737dd6f5221f6ad04b4d12
jahidul <jhahedulislam420@gmail.com>
2022-07-04 02:47:55 +0600

    first commit

---
+++ src/images.js
@@ -0,0 +1,9 @@
+import image1 from './assets/1.jpg'
+import image3 from './assets/3.jpg'
+import image4 from './assets/4.jpg'
+import image5 from './assets/5.jpg'
+import image6 from './assets/6.jpg'
+import image7 from './assets/7.jpg'
+
+const imageArr = [image1, image3, image4, image5, image6, image7];
+export default imageArr;
